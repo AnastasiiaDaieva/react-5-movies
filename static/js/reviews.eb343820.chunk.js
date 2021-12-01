1a5dc7e514ecc8ec473fdf0536429fed5c334e48
eche-unicorn <d.anasteysha@gmail.com>
2021-12-01 23:17:34 +0200

    Updates

---
+++ static/js/reviews.eb343820.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-5-movies"]=this["webpackJsonpreact-5-movies"]||[]).push([[6],{87:function(e,r,s){"use strict";s.r(r),s.d(r,"default",(function(){return i}));var t=s(44),c=(s(0),s(48),s(45),s(2));function i(e){var r=e.reviewsArray;return console.log("reviewsArray",r),Object(c.jsx)(t.a,{children:Object(c.jsx)("p",{children:"Reviews"})})}}}]);
+//# sourceMappingURL=reviews.eb343820.chunk.js.map
